e90c631c35efee7d41dee9b3ef4298ed78b000dc
Alfredo <alfredo.solano@the-cocktail.com>
2010-04-18 18:36:10 +0200

    Added Position To Attachings to reorder assets

--- lib/acts_as_polymorphic_paperclip.rb
+++ lib/acts_as_polymorphic_paperclip.rb
@@ -4,10 +4,10 @@ module LocusFocus
       def self.included(base)
         base.extend ClassMethods
       end
-
+      
       module ClassMethods
         # Extends the model to afford the ability to associate other records with the receiving record.
-        #
+        # 
         # This module needs the paperclip plugin to work
         # http://www.thoughtbot.com/projects/paperclip
         def acts_as_polymorphic_paperclip(options = {})
@@ -17,33 +17,33 @@ module LocusFocus
           })
           class_inheritable_reader :acts_as_polymorphic_paperclip_options
 
-          has_many :attachings, :as => :attachable, :dependent => :destroy
-          has_many :assets, :through => :attachings do
+            has_many :attachings, :as => :attachable,:order => :position, :dependent => :destroy
+            has_many :assets,:order => :position,:through => :attachings do
             def attach(asset_id)
               asset_id = extract_id(asset_id)
               asset = Asset.find(asset_id)
               @owner.assets << asset
               @owner.assets(true)
             end
-
+            
             def detach(asset_id, delete_if_no_attachings = false)
               asset_id = extract_id(asset_id)
               attaching = @owner.attachings.find(:first, :conditions => ['asset_id = ?', asset_id])
               attachable = attaching.attachable
               raise ActiveRecord::RecordNotFound unless attaching
               result = attaching.destroy
-
+              
               asset = Asset.find(asset_id)
               if asset.attachings.empty? && delete_if_no_attachings# delete if no longer attached to anything
                 override_default_styles, normalised_styles = attachable.override_default_styles?(asset.name)
                 asset.data.instance_variable_set("@styles", normalised_styles) if override_default_styles
                 asset.data.send(:queue_existing_for_delete)
                 asset.data.send(:flush_deletes)
-                asset.save # needed to permanently remove file name and urls
+                asset.save # needed to permanently remove file name and urls 
               end
               result
             end
-
+            
             protected
             def extract_id(obj)
               return obj.id unless obj.class == Fixnum || obj.class == String
@@ -57,7 +57,7 @@ module LocusFocus
           # original_filename: 64x16.png
           # original_path: 64x16.png
           attr_accessor :data
-
+            
           include LocusFocus::Acts::PolymorphicPaperclip::InstanceMethods
         end
       end
@@ -74,15 +74,15 @@ module LocusFocus
             end
           end unless data.nil? || data.blank?
         end
-
+        
         def create_and_save_asset(data_item)
-          the_asset = Asset.new(:data_file_name => data_item.original_filename)
+          the_asset = Asset.find_or_initialize_by_data_file_name(data_item.original_filename)
           override_default_styles, normalised_styles = override_default_styles?(data_item.original_filename)
           the_asset.data.instance_variable_set("@styles", normalised_styles) if override_default_styles
           the_asset.data = data_item
           if the_asset.save
-
-            # This association may be saved more than once within the same request / response
+  
+            # This association may be saved more than once within the same request / response 
             # cycle, which leads to needless DB calls. Now we'll clear out the data attribute
             # once the record is successfully saved any subsequent calls will be ignored.
             data_item = nil
@@ -115,4 +115,3 @@ module LocusFocus
     end
   end
 end
-
